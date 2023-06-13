9ebb6ffdf26be4a75d375639557b3a0ba058344c
Diogo Passos Perdigão Britto <diogopbritto@gmail.com>
2023-06-13 19:51:25 -0300

    server up

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-import express from "express"; //Includes básicos do express
-import http from "http";
-import swaggerUi from "swagger-ui-express";
-import fs from "fs";
-import yaml from "yaml";
-import createError from "http-errors"; //Usando para enviar erro 404
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from 'express'; //Includes básicos do express
+import http from 'http';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import yaml from 'yaml';
+import createError from 'http-errors'; //Usando para enviar erro 404
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import favicon from "serve-favicon"; //Para debug no navegador, nem um pouco necessário
-
-import routers from "./routes";
+import routers from './routes';
 
 /**
  * Classe principal da aplicação
@@ -30,11 +28,11 @@ class App {
 	 * Adiciona os middlewares
 	 */
 	middlewares() {
-		this.app.use(morgan("dev")); //tratamentos de http
+		this.app.use(morgan('dev')); //tratamentos de http
 		this.app.use(express.json()); //reconhece solicitação de entrada como json
 		this.app.use(express.urlencoded({ extended: false })); //reconhece o objeto de solicitação recebido como cadeias ou matrizes
 		let allowedOrigins = [
-			"localhost:8080", // sem o http para f5 desenvolvimento local
+			'localhost:8080', // sem o http para f5 desenvolvimento local
 		];
 		// Permitir varias origens no navegador, usado mais para desenvolvimento, embora acredito que sera usado para produção
 		this.app.use((req, res, next) => {
@@ -42,19 +40,16 @@ class App {
 			console.log(origin);
 
 			if (allowedOrigins.indexOf(origin) > -1) {
-				res.setHeader("Access-Control-Allow-Origin", origin);
-				res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-				res.header(
-					"Access-Control-Allow-Headers",
-					"Content-Type, Authorization"
-				);
-				res.header("Access-Control-Allow-Credentials", true);
+				res.setHeader('Access-Control-Allow-Origin', origin);
+				res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+				res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+				res.header('Access-Control-Allow-Credentials', true);
 				return next();
 			} else {
 				console.log(origin);
 				console.log(allowedOrigins.indexOf(origin));
-				res.setHeader("Access-Control-Allow-Origin", allowedOrigins[0]);
-				res.status(403).send("Forbidden");
+				res.setHeader('Access-Control-Allow-Origin', allowedOrigins[0]);
+				res.status(403).send('Forbidden');
 			}
 		});
 	}
@@ -64,7 +59,7 @@ class App {
 	 */
 	routes() {
 		// GET /version draw version from package.json
-		this.app.get("/version", (req, res) => {
+		this.app.get('/version', (req, res) => {
 			try {
 				res.status(200).send(process.env.VERSION);
 			} catch (error) {
@@ -73,30 +68,25 @@ class App {
 		});
 		// GET /api-docs docs da api swagger
 		this.app.use(
-			"/api-docs",
+			'/api-docs',
 			swaggerUi.serve,
-			swaggerUi.setup(
-				yaml.parse(fs.readFileSync("./routes/swagger.yaml", "utf8"))
-			)
+			swaggerUi.setup(yaml.parse(fs.readFileSync('./routes/swagger.yaml', 'utf8')))
 		);
 		routers(this.app);
 		//#endregion
 
-		//Ordem especifica, 3º pasta publica e favicon
+		//Ordem especifica, 3º pasta publica
 		const __dirname = path.dirname(fileURLToPath(import.meta.url));
-		this.app.use(express.static(path.resolve(__dirname, "/routes/public"))); //Rota para a pasta public para css
-		this.app.use(favicon(__dirname + "/routes/public/favicon.ico")); //Somente para dev
+		this.app.use(express.static(path.resolve(__dirname, '/routes/public'))); //Rota para a pasta public para css
 
 		//Ordem especifica, 4º error handler 404
 		this.app.use((err, req, res, next) => {
 			// set locals, not providing error in production
 			res.locals.message = err.message;
-			res.locals.error = process.env.NODE_ENV === "production" ? {} : err;
-			console.warn("envolviment: " + process.env.NODE_ENV);
-			console.warn(
-				req.headers["x-forwarded-for"] || req.connection.remoteAddress
-			);
-			res.status(err.status || 500).send("error");
+			res.locals.error = process.env.NODE_ENV === 'production' ? {} : err;
+			console.warn('envolviment: ' + process.env.NODE_ENV);
+			console.warn(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+			res.status(err.status || 500).send('error');
 		});
 
 		//Ordem especifica, 5º Manipuladores de errors catch 404 and forward to error handler
